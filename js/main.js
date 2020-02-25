8c6bf2f11687ab98e4f321e89f9f9741a0a2df6c
Valdas Urbonas <kibitaro58@gmail.com>
2020-02-25 23:46:40 +0200

    Testing somestuf

--- js/main.js
+++ js/main.js
@@ -1,19 +1,71 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var scene, camera, render;
+const mouse = new THREE.Vector2();
+const target = new THREE.Vector2();
+class Boid{
+    constructor(object, name, id){
+        this.object = object;
+        this.name = name;
+        this.id = id;
+    }
+}
 
-camera.position.z = 5;
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff );
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshNormalMaterial();
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    document.addEventListener( 'mousemove', onMouseMove, true );
+    window.addEventListener( 'resize', onResize, false );
+
+    let boidai = [];
+    addBoids();
+function addBoids () {
+    for(var i = 0; i < 30; i++){
+        boidai[i]= new Boid(new THREE.Mesh(geometry,material),"Boidas" + i,i);
+        boidai[i].object.position.x = 2*i;
+        scene.add(boidai[i].object);
+    }
+}
+
+function onMouseMove( event ) {
+
+	mouse.x = ( event.clientX - windowHalf.x );
+	mouse.y = ( event.clientY - windowHalf.x );
+
+}
+
+function onResize( event ) {
+
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+  
+    windowHalf.set( width / 2, height / 2 );
+	
+    camera.aspect = width / height;
+	camera.updateProjectionMatrix();
+	renderer.setSize( width, height );
+				
+}
+
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    for(var i = 0; i < boidai.length; i++){
+        boidai[i].object.rotation.x += 0.05;
+        boidai[i].object.rotation.y += 0.05;
+    }
+    target.x = ( 1 - mouse.x ) * 0.002;
+    target.y = ( 1 - mouse.y ) * 0.002;
+  
+    camera.rotation.x += 0.05 * ( target.y - camera.rotation.x );
+    camera.rotation.y += 0.05 * ( target.x - camera.rotation.y );
     renderer.render(scene, camera);
 }
 animate();
