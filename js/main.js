4a544f5f22c67d602a19d49f5c92ac7dfa5de7d6
Valdas Urbonas <kibitaro58@gmail.com>
2020-02-25 20:49:30 +0200

    Images

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ scene.add(cube);
 camera.position.z = 5;
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.5;
+    cube.rotation.y += 0.5;
     renderer.render(scene, camera);
 }
 animate();
