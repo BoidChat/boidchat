16ae07ea57c689732ddc10d54692f744afae6cf1
mataniakas <mataniakas@gmail.com>
2020-03-09 12:20:22 +0200

    added angular.js

---
+++ public/keyup.component.ts
@@ -0,0 +1,14 @@
+// @Component({
+//   selector: 'app-key-up',
+//   templateUrl: `./keyup.component.html`
+// })
+// export class KeyUpComponent {
+
+//   values = [];
+//   onEnter(value: string) {
+  
+//   for(var i=value.length;i>0;i--){
+//     this.values[i]=this.values[i-1];
+//   }
+//   this.values[0] = value; }
+// }
