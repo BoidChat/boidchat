d541550c9df21fd7b78779e905ace7124c290987
gintonas <gintonasgn@gmail.com>
2020-04-14 15:22:12 +0300

    readability refactoring on a file

--- TextMesg/src/app/keyup/keyup.component.ts
+++ TextMesg/src/app/keyup/keyup.component.ts
@@ -3,7 +3,9 @@ import io from 'socket.io';
 const socket=io.connect();
 
 let messages: Array<String>;
+
 @Component({
+
   selector: 'app-keyup',
   templateUrl: './keyup.component.html',
   styleUrls: ['./keyup.component.css']
@@ -16,24 +18,29 @@ export class KeyupComponent implements OnInit {
   }
 
   onEnter(value: string) {
+
     if(value.length>256){
       //exception message?
     }
     else{
+
       io.emit('message',value);
+
       for(var i=value.length;i>0;i--){
         messages[i]=messages[i-1];
       }
+
       messages[0] = value;
-      
-      
+    
     }
   }
-  
 }
+
 socket.on('message',function(data){
+
   for(var i=this.messages.length;i>0;i--){
     this.messages[i]=this.messages[i-1];
   }
+  
   this.messages[0] = data;
 });
