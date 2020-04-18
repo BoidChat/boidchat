6fc52dafb4abb33b7f9512326aa79a8597477aef
linas <linas7879@gmail.com>
2020-04-18 22:28:07 +0300

    clusterization bug fixed

--- public/BoidSystem.js
+++ public/BoidSystem.js
@@ -1,7 +1,7 @@
 const max_velocity = 0.5;
-// const avg_velocity = 0.35;
 const min_velocity = 0.2;
-const base_free_movement_radius = 60; //radius where icentric force(not limited by max_force) starts
+const base_free_movement_radius = 50; //radius where icentric force(not limited by max_force) starts
+// const base_free_movement_radius = 20; //radius where icentric force(not limited by max_force) starts
 // const free_movement_radius = 20; //radius where icentric force(not limited by max_force) starts
 const max_force = 0.03;
 const noise_const = 1 / 10;
@@ -12,7 +12,7 @@ function Boid(geom, base) {
 	// this.position = rand_vect(20); //need changes
 	this.velocity = rand_vect(0.5);
 	this.acceleration = new THREE.Vector3();
-	// this.perception = 6;
+	// this.perception = 10;
 	this.perception = 8;
 	this.geom = geom;
 	this.geom.position.set(this.position.x, this.position.y, this.position.z);
@@ -45,10 +45,10 @@ Boid.prototype.live = function(data) {
 	this.velocity.add(this.acceleration);
 	// this.velocity = normalize_vect(this.velocity, max_velocity);
 	this.velocity = normalize_vect2(this.velocity, max_velocity, min_velocity);
+	this.update_neighbors(data);
 	this.position.add(this.velocity);
 	this.geom.position.set(this.position.x, this.position.y, this.position.z);
 	this.acceleration.set(0, 0, 0);
-	this.update_neighbors(data);
 	return {
 		id: this.id,
 		name: this.name,
@@ -94,20 +94,18 @@ Boid.prototype.apply_alignment = function(data) {
 };
 
 Boid.prototype.apply_separation = function(data) {
-	let all_vectors = new THREE.Vector3();
+	let all_vectors = [0, 0, 0];
 	let count = 0;
 	let position = this.position.toArray();
 	for (let i = 0; i < data.length; i++) {
-		if (distance(position, data[i].position) < this.perception && data[i].id != this.id) {
-			vect_from_me = to_vector3(data[i].position).sub(this.position);
-			let multiplier = (vect_from_me.length() / this.perception) ** 2;
-			all_vectors.add(vect_from_me.multiplyScalar(multiplier));
+		if (distance(position, data[i].position) < this.perception / 2 && data[i].id != this.id) {
+			add_to(all_vectors, data[i].position);
 			count++;
 		}
 	}
-	if (count != 0) {
-		let from_center = all_vectors.divideScalar(-count);
-		this.acceleration.add(normalize_vect(from_center, max_force));
+	if (count > 0) {
+		let to_center = to_vector3(divide_scalar(all_vectors, count)).sub(this.position);
+		this.acceleration.add(streach_vect(to_center, -max_force));
 	}
 };
 
@@ -123,7 +121,7 @@ Boid.prototype.apply_cohesion = function(data) {
 	}
 	if (count > 0) {
 		let to_center = to_vector3(divide_scalar(all_vectors, count)).sub(this.position);
-		this.acceleration.add(normalize_vect(to_center, max_force * 0.7));
+		this.acceleration.add(normalize_vect(to_center, max_force * 0.5));
 	}
 };
 
@@ -137,7 +135,7 @@ Boid.prototype.apply_attraction_to_center = function(count) {
 		free_movement_radius = Math.pow((free_movement_coafitient * count * Math.pow(this.perception, 3)), 1 / 3);
 	}
 	let dist_to_center = this.position.length();
-	let outer_radius = free_movement_radius * 0.2; //coordinate center should be (0 0 0)
+	let outer_radius = free_movement_radius * 0.3; //coordinate center should be (0 0 0)
 	if (dist_to_center > free_movement_radius) {
 		let diff = free_movement_radius - dist_to_center;
 		let d = normalize_vect(this.position.clone(), max_force * (diff / outer_radius));
