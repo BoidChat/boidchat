19bbcb32126aad34d680640afc02e36442089eb1
gintonas <gintonasgn@gmail.com>
2020-04-18 10:21:31 +0300

    more testing on integration

--- TextMesg/src/app/app.module.ts
+++ TextMesg/src/app/app.module.ts
@@ -4,24 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KeyupComponent } from './keyup/keyup.component';
-import { BoidSystem } from '../assets/public/BoidSystem.js';
-import { ExtraMath } from '../assets/public/extraMath.js';
-import { Sketch } from '../assets/public/sketch.js';
+import { BoidSystem } from '../assets/js/BoidSystem.js';
+import { ExtraMath } from '../assets/js/extraMath.js';
+import { Sketch } from '../assets/js/sketch.js';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     KeyupComponent,
-    // BoidSystem, //not sure how to import without problems
-    // ExtraMath,
-    // Sketch
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    BoidSystem, //not sure how to import without problems
+    ExtraMath,
+    Sketch
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
