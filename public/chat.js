e379e1b81c56863f52601d9fcfe7570027160ce1
Valdas Urbonas <kibitaro58@gmail.com>
2020-05-09 11:47:01 +0300

    some css fixes ir pagrazinimas

--- public/chat.js
+++ public/chat.js
@@ -1,13 +1,17 @@
 function sendMessage(evt) {
 	if (evt.keyCode === 13) {
-		socket.emit('send_message', evt.target.value);
+		if(evt.target.value != "")
+			socket.emit('send_message', evt.target.value);
 		evt.target.value = "";
 	}
 }
 
 function sendButton(){
-	socket.emit('send_message', document.getElementById("TextBox").value);
-	document.getElementById("TextBox").value = "";
+	if(document.getElementById("textBox").value != "")
+	{
+		socket.emit('send_message', document.getElementById("textBox").value);
+	}
+	document.getElementById("textBox").value = "";
 }
 
 socket.on('receive_message', (data, name) => {
@@ -19,6 +23,7 @@ socket.on('receive_message', (data, name) => {
 	node.appendChild(sender);
 	document.getElementById("chatBox").appendChild(node);
 });
+
 
 
 
