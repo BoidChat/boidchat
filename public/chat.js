6e1fdd7ccccbaec1833961272ee994af567b343b
linas <linas7879@gmail.com>
2020-05-10 11:48:20 +0300

    basic link detection added

--- public/chat.js
+++ public/chat.js
@@ -17,12 +17,17 @@ function sendButton(){
 socket.on('receive_message', (data, name) => {
 	var node = document.createElement("p");
 	node.id = "chat";
-	var sender = document.createTextNode(name);
-	sender.appendData(": ");
-	sender.appendData(data);
-	node.appendChild(sender);
+	let marked_text = name + ": " + linkify(data);
+	node.innerHTML = marked_text;
 	document.getElementById("chatBox").appendChild(node);
 });
+
+function linkify(text, element) {
+    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(urlRegex, function(url) {
+        return '<a href="' + url + '">' + url + '</a>';
+    });
+}
 
 
 
