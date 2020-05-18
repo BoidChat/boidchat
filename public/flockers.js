243c6b36c5499f1ebf5566ccba305c361cd2f63f
Valdas Urbonas <kibitaro58@gmail.com>
2020-05-18 21:03:35 +0300

    flockers boost

--- public/flockers.js
+++ public/flockers.js
@@ -4,9 +4,18 @@ socket.on('cluster_info', (data) => {
 		parent.removeChild(parent.lastChild);
 	}
 	for (let i = 0; i < data.length; i++) {
+		let div = document.createElement("div");
+		div.id = "flockers" + i;
 		let node = document.createElement("p");
-		node.id = "flockers" + i;
 		node.innerHTML = '<font color="FFFFFF">' + data[i];
-		document.getElementById("flockers").appendChild(node);
+		node.style.display = "inline-block";
+		div.appendChild(node);
+		if(i != -1){
+			let bl = document.createElement("button");
+			bl.id = "blockButton";
+			bl.textContent = "Blokuoti";
+			div.appendChild(bl);
+		}
+		document.getElementById("flockers").appendChild(div);
 	}
 });
