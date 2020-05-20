84231bc8104f22bfd08b17de13677b5413db4278
Valdas Urbonas <kibitaro58@gmail.com>
2020-05-20 15:40:47 +0300

    chat time

--- public/chat.js
+++ public/chat.js
@@ -15,19 +15,32 @@ function sendButton(){
 }
 
 socket.on('receive_message', (data, name) => {
-	var node = document.createElement("p");
-	node.id = "message";
+	let div = document.createElement("div");
+	let node = document.createElement("p");
+	let time = document.createElement("p");
+	node.id = "messageText";
 	let marked_text = name + ": " + linkify(data);
+	let today = new Date();
+	let h = today.getHours();
+	let m = today.getMinutes();
+	m = checkTime(m);
+	time.innerText = h + ":" + m;
 	node.innerHTML = marked_text;
-	var chat = document.getElementById("chatBox");
-	var toScroll = chat.scrollTop + chat.clientHeight - chat.scrollHeight;
-	chat.appendChild(node);
+	let chat = document.getElementById("chatBox");
+	let toScroll = chat.scrollTop + chat.clientHeight - chat.scrollHeight;
+	div.appendChild(node);
+	div.appendChild(time);
+	chat.appendChild(div);
 	if(toScroll > -20 && toScroll < 20)
 		chat.scrollTop = chat.scrollHeight;
 });
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+	return i;
+}
 
 function linkify(text, element) {
-    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    let urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
     return text.replace(urlRegex, function(url) {
         return '<a href="' + url + '">' + url + '</a>';
     });
@@ -35,7 +48,7 @@ function linkify(text, element) {
 
 function hideChat(){
 	document.getElementById("chatBox").style.visibility = "hidden";
-	var head = document.getElementById("chatHeader");
+	let head = document.getElementById("chatHeader");
 	head.style.bottom = document.getElementById("textBox").offsetHeight + "px";
 	document.getElementById("minmize").style.visibility = "hidden";
 	document.getElementById("maximize").style.visibility = "visible";
@@ -44,7 +57,7 @@ function hideChat(){
 
 function showChat(){
 	document.getElementById("chatBox").style.visibility = "visible";
-	var head = document.getElementById("chatHeader");
+	let head = document.getElementById("chatHeader");
 	head.style.bottom = document.getElementById("chatBox").offsetHeight + head.offsetHeight +"px" ;
 	document.getElementById("minmize").style.visibility = "visible";
 	document.getElementById("maximize").style.visibility = "hidden";
@@ -53,7 +66,7 @@ function showChat(){
 window.addEventListener('resize', onWindowResize, false);
 
 function onWindowResize(){
-	var head = document.getElementById("chatHeader");
+	let head = document.getElementById("chatHeader");
 	if(document.getElementById("chatBox").style.getPropertyValue("visibility") == "hidden"){
 		head.style.bottom = document.getElementById("textBox").offsetHeight + "px";
 	}
